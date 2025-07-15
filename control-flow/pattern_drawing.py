68524a80c09af73d39b6fb7ab8c9df0acfae04a1
Gelagay G. <gela.sirta3@gmail.com>
2025-07-15 15:49:36 +0300

    Nested while and for loops to create squared shaped pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,9 @@
 num_of_pattern = int(input("Enter the size of the pattern:"))
-for i in range(num_of_pattern):
+
+
+i = 0
+while i < num_of_pattern :
     for j in range(num_of_pattern):
-        print("*", end="")
+        print('*', end='')
     print()
+    i += 1
