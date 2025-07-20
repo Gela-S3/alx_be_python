c3659207126abdc422e7c0ef48534eb91a7d90c4
Gelagay G. <gela.sirta3@gmail.com>
2025-07-20 23:02:00 +0300

    Daily reminder modified.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,18 +4,18 @@ time_bound = input("Is it time-bound? (yes/no):")
 if time_bound == "yes":
     match priority:
         case "high":
-            print(f"Reminder : {task} is {priority.lower()} priority task that requiers immediate attention today!")
+            print(f"Reminder: {task} is {priority.lower()} priority task that requiers immediate attention today!")
         case "medium":
-            print(f"Reminder : {task} is {priority.lower()} priority task,but complete it when you have free time.")
+            print(f"Reminder: {task} is {priority.lower()} priority task,but complete it when you have free time.")
         case "low":
-            print(f"Reminder : {task} is {priority.lower()} priority task.Try to complete with in give time_bound")
+            print(f"Reminder: {task} is {priority.lower()} priority task.Try to complete with in give time_bound")
 elif time_bound == "no":
     match priority:
         case "high":
-            print(f"Reminder : {task} is {priority.lower()} priority task.Give attention!")
+            print(f"Reminder: {task} is {priority.lower()} priority task.Give attention!")
         case "medium" :
-            print(f"Reminder : {task} is {priority.lower()} priority task.Calm down! You have time to {task}")
+            print(f"Reminder: {task} is {priority.lower()} priority task.Calm down! You have time to {task}")
         case "low" :
-            print(f"Reminder : {task} is {priority.lower()} priority task.Consider completing it when you have free time.")
+            print(f"Reminder: {task} is {priority.lower()} priority task.Consider completing it when you have free time.")
 else:
     print("Please give a correct answer.")
